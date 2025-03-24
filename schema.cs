785f5b24dc46c34fa1893d2d16193c2cc07789d8
Francisco Susana <37332293+franciscosuca@users.noreply.github.com>
2025-03-24 20:47:38 +0100

    Create schema for the data stored on the DB.

---
+++ schema.cs
@@ -0,0 +1,13 @@
+namespace Handler;
+
+public class Experience
+{
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public DateOnly Sdate { get; set; }
+    public DateOnly Edate { get; set; }
+    public string Company { get; set; }
+    public string Location { get; set; }
+    public string Summary { get; set; }
+    public string sampleskills { get; set; }
+}
