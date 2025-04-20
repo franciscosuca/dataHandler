e17e338449f730d2e51af3449ab05371c62be167
Francisco Susana <37332293+franciscosuca@users.noreply.github.com>
2025-04-20 20:26:17 +0200

    chore: attempt to trigger the function when an experience is removed from the blob container.

--- contentTrigger.cs
+++ contentTrigger.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Options;
 using personalSite.Models.Entities;
 using System.Text.Json;
 using personalSite.Services;
+using Microsoft.WindowsAzure.Storage.Blob.Protocol;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace personalSite
 {
@@ -28,17 +30,35 @@ namespace personalSite
         }
 
         [FunctionName(nameof(contentTrigger))]
-        public async Task Run([BlobTrigger("samples-workitems/{name}", Connection = "f6bc32_STORAGE")] Stream stream, string name)
+        public async Task Run(
+            [BlobTrigger("samples-workitems/{name}", Connection = "f6bc32_STORAGE")] Stream stream,
+            string name,
+            IDictionary<string, string> metadata, //TODO: remove this argument if is not needed.
+            BlobTriggerAttribute attr) //TODO: remove this argument if is not needed.
         {
+            ExperienceHandler experienceHandler = new ExperienceHandler(_cosmosDb, _configuration.ContainerName, _loggerFactory);
+
+            // Check if this is a deletion event (stream will be empty)
+            if (stream.Length == 0)
+            {
+                _logger.LogInformation($"Blob {name} was deleted. Removing corresponding experience...");
+                // Since the blob is deleted, we need to find and remove the corresponding experience
+                // You might need to implement a way to get the Experience ID from the blob name
+                //TODO: adjust this so the correct experience is removed from the dB
+                var experience = new Experience { id = name };
+                //await experienceHandler.Remover(experience);
+                return;
+            }
+
             using var blobStreamReader = new StreamReader(stream);
             var content = await blobStreamReader.ReadToEndAsync();
-            ExperienceHandler experienceHandler = new ExperienceHandler(_cosmosDb, _configuration.ContainerName, _loggerFactory);
 
             string? BlobConnectionString = Environment.GetEnvironmentVariable("f6bc32_STORAGE");
             if (string.IsNullOrEmpty(BlobConnectionString))
             {
                 throw new Exception("No Connection String for the blob container found.");
             }
+
             BlobService blobService = new(BlobConnectionString, "samples-workitems");
 
             try
